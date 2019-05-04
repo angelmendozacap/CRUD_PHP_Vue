e29858717a71f7cb8716ceb6b6cbe08b9f645d02
Alfredo Mendoza <kick-1930@hotmail.com>
2019-05-04 18:11:31 -0500

    Se terminó la funcionalidad del CRUD

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,23 @@
 ;
+
+const query = async (method = 'GET', body = [], params = 'read') => {
+  try {
+    const API_URL = `./api.php?action=${params}`
+    let myInit = {}
+    if (method !== 'GET') {
+      myInit = {
+        method,
+        body
+      }
+    }
+    const res = await fetch(API_URL, myInit)
+    const data = await res.json()
+    return data
+  } catch (err) {
+    console.log(err)
+  }
+}
+
 const app = new Vue({
   el: '#app',
   data: {
@@ -6,12 +25,73 @@ const app = new Vue({
     showAddModal: false,
     showEditModal: false,
     showDeleteModal: false,
-    erroMessage: '',
+    errorMessage: '',
     successMessage: '',
     students: [],
     activeStudent: {},
   },
-  mounted: {
-
+  mounted() {
+    this.getAllStudents()
+  },
+  computed: {
+    displayAddModal() {
+      return (this.showAddModal) ? 'u-show' : ''
+    },
+    displayEditModal() {
+      return (this.showEditModal) ? 'u-show' : ''
+    },
+    displayDeleteModal() {
+      return (this.showDeleteModal) ? 'u-show' : ''
+    },
+  },
+  methods: {
+    toggleModal(modal) {
+      if (modal === 'add') {
+        this.showAddModal = !this.showAddModal
+      } else if (modal === 'edit') {
+        this.showEditModal = !this.showEditModal
+      } else if (modal === 'delete') {
+        this.showDeleteModal = !this.showDeleteModal
+      }
+    },
+    setMessage(res) {
+      if (res.code === 201 || res.code === 200) {
+        console.log(res)
+        this.successMessage = res.message
+        this.getAllStudents()
+      } else {
+        this.errorMessage = res.message
+      }
+      setTimeout(() => {
+        this.errorMessage = false
+        this.successMessage = false
+      }, 3000);
+    },
+    getAllStudents: async function() {
+      const students = await query()
+      this.students = students
+    },
+    createStudent: async function (e) {
+      const formData = new FormData(e.target)
+      const data = await query('POST', formData, 'create')
+      this.toggleModal('add')
+      this.setMessage(data)      
+    },
+    getStudent(action, student) {
+      this.toggleModal(action)
+      this.activeStudent = student
+    },
+    updateStudent: async function (e) {
+      const formData = new FormData(e.target)
+      const data = await query('POST', formData, 'update')
+      this.toggleModal('edit')
+      this.setMessage(data)
+    },
+    deleteStudent: async function(e) {
+      const formData = new FormData(e.target)
+      const data = await query('POST', formData, 'delete')
+      this.toggleModal('delete')
+      this.setMessage(data)
+    },
   }
 })
